25b59f08596d0f41fdddec6b45dd2a890717f0e9
pancake <pancake@nopcode.org>
2025-02-21 04:29:19 +0100

    Single AppDelegate

--- Sources/YarnTwix/AppDelegate.swift
+++ Sources/YarnTwix/AppDelegate.swift
@@ -1,25 +1,45 @@
 import Cocoa
 import SwiftUI
 
-class AAppDelegate: NSObject, NSApplicationDelegate {
+@objc class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         let contentView = MainView()
-        // Create the window
+
         window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+            contentRect: NSRect(x: 0, y: 0, width: 1000, height: 600),
             styleMask: [.titled, .closable, .resizable, .miniaturizable],
-            backing: .buffered,
-            defer: false
-        )
+            backing: .buffered, defer: false)
         window.center()
-        window.setFrameAutosaveName("Main Window")
+        window.title = "YarnTwix"
         window.contentView = NSHostingView(rootView: contentView)
         window.makeKeyAndOrderFront(nil)
+
+        setupMenus()
     }
 
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
+    func setupMenus() {
+        let mainMenu = NSMenu()
+        NSApp.mainMenu = mainMenu
+
+        let appMenuItem = NSMenuItem()
+        mainMenu.addItem(appMenuItem)
+
+        let appMenu = NSMenu()
+        appMenuItem.submenu = appMenu
+
+        let newPostMenuItem = NSMenuItem(title: "New Post", action: #selector(newPostAction(_:)), keyEquivalent: "n")
+        newPostMenuItem.target = self
+        appMenu.addItem(newPostMenuItem)
+
+        appMenu.addItem(NSMenuItem.separator())
+
+        let quitMenuItem = NSMenuItem(title: "Quit Twtxt", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        appMenu.addItem(quitMenuItem)
+    }
+
+    @objc func newPostAction(_ sender: Any?) {
+        NotificationCenter.default.post(name: .newPost, object: nil)
     }
 }
--- Sources/YarnTwix/main.swift
+++ Sources/YarnTwix/main.swift
@@ -3,56 +3,6 @@ import AppKit
 import Cocoa
 import SwiftUI
 
-@objc class AppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow!
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Create the SwiftUI view that provides the window content.
-        let contentView = MainView()
-
-        // Create the window with a title and style.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 1000, height: 600),
-            styleMask: [.titled, .closable, .resizable, .miniaturizable],
-            backing: .buffered, defer: false)
-        window.center()
-        window.title = "YarnTwix"
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
-
-        // Setup the application menu.
-        setupMenus()
-    }
-
-    func setupMenus() {
-        let mainMenu = NSMenu()
-        NSApp.mainMenu = mainMenu
-
-        // Application menu item (the first menu in the menu bar)
-        let appMenuItem = NSMenuItem()
-        mainMenu.addItem(appMenuItem)
-
-        let appMenu = NSMenu()
-        appMenuItem.submenu = appMenu
-
-        // "New Post" menu item with cmd+N.
-        let newPostMenuItem = NSMenuItem(title: "New Post", action: #selector(newPostAction(_:)), keyEquivalent: "n")
-        newPostMenuItem.target = self
-        appMenu.addItem(newPostMenuItem)
-
-        // Separator for clarity.
-        appMenu.addItem(NSMenuItem.separator())
-
-        // "Quit" menu item with cmd+Q.
-        let quitMenuItem = NSMenuItem(title: "Quit Twtxt", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
-        appMenu.addItem(quitMenuItem)
-    }
-
-    @objc func newPostAction(_ sender: Any?) {
-        // Post a notification that your SwiftUI MainView listens for to show the new post dialog.
-        NotificationCenter.default.post(name: .newPost, object: nil)
-    }
-}
 
 extension Notification.Name {
     static let newPost = Notification.Name("newPost")
